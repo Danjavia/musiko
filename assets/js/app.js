a07bb03ddf6f0b550da092b2c4ea1699a9ad9a74
Danjavia <danjavia.iconshock@gmail.com>
2015-11-14 00:27:15 -0500

    test de pruebas #@ Obtaining matches

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,42 @@
 'use strict';
 
-// var API_LAST_KEY = "a8da4176b3e227778d267fdc4df7ab36";
-// var API_LAST_URL = "http://ws.audioscrobbler.com/2.0/?";
-
 var SearchBox = React.createClass({
 
-	getInitialState: function () {
-		return {
-			API_LAST_KEY: "a8da4176b3e227778d267fdc4df7ab36",
-			API_LAST_URL: "http://ws.audioscrobbler.com/2.0/?"
-		};
-	},
-
 	search: function ( e ) {
+
 		e.preventDefault();
-		console.log( 'form sended', this.props.API_LAST_URL );
+
+		var querySearch = this.refs.search.value;
+
+		var queryData = { 
+			method: 'artist.search', 
+			api_key: this.props.lastKey, 
+			artist: querySearch, 
+			limit: 8,
+			format: 'json'
+		};
+
+		console.log( queryData );
+
+		$.ajax({
+
+      		url: this.props.url,
+      		dataType: 'json',
+      		type: 'POST',
+      		data: queryData,
+
+      		success: function( data ) {
+      			console.log( data );
+      		  	this.setState({ result: data });
+      		}.bind( this ),
+
+      		error: function( xhr, status, err ) {
+      			console.log( status );
+      		  	console.error( this.props.url, status, err.toString() );
+      		}.bind( this )
+    	});
+
+		console.log( 'form sended', querySearch, this.props.url,  this.props.lastKey );
 	},
 
 	render: function () {
@@ -22,7 +44,7 @@ var SearchBox = React.createClass({
 			<div className="searchBox">
 				<form action="#" className="form-inline" onSubmit={this.search}>
 					<div className="form-group">
-						<input type="text" className="form-control" placeholder="Your name" ref="author" />
+						<input type="text" className="form-control" placeholder="Your name" ref="search" />
 						<button type="submit" className="btn btn-success input-group">Buscar</button>
 					</div>
 				</form>
@@ -31,7 +53,36 @@ var SearchBox = React.createClass({
 	}
 });
 
+var Results = React.createClass({
+	
+	render: function () {
+		return (
+			<div className="result">Hello {this.props.name}</div>
+		)
+	} 
+})
+
+var Musiko = React.createClass({
+
+	getInitialState: function () {
+
+		return {
+			API_LAST_URL: "http://ws.audioscrobbler.com/2.0/",
+			API_LAST_KEY: "a8da4176b3e227778d267fdc4df7ab36"
+		};
+	},
+	
+	render: function () {
+		return (
+			<div className="musiko">
+				<SearchBox url={this.state.API_LAST_URL} lastKey={this.state.API_LAST_KEY}/>
+				<Results name="Danny viasus"/>
+			</div>
+		)
+	}
+})
+
 ReactDOM.render(
-  	<SearchBox/>,
+  	<Musiko/>,
   	document.getElementById( 'content' )
 );
